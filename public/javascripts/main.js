1ed42cdf08d0248762a0ccf46a523aacf9bb0faa
Pastor <ascci.gcc@gmail.com>
2014-07-23 13:37:44 -0500

    Added the fix to room in each request

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,13 +1,32 @@
+// General functions
+var rand = function() {
+    return Math.random().toString(36).substr(2); // remove `0.`
+};
+
+var token = function() {
+    return rand() + rand(); // to make it longer
+};
+
+
+// Socket io connection
+
 var socket = io.connect(window.location.hostname);
 
-socket.on('status', function (data) {
-    console.log(data.status);
-    $('#output').text(data.status);
+//var room = 'axc12';
+var room = token();
+
+socket.on('connect', function() {
+  socket.emit('room', room);
+});
+
+socket.on('output', function (data) {
+  console.log(data.status);
+  $('#output').text(data.status);
 });
 
 $('#input').on('change keyup paste', function(){
     var c = $('#input').val();
-    socket.emit('reset', c);
+    socket.emit('input', c, room);
 });
 
 
--- server/index.js
+++ server/index.js
@@ -4,9 +4,12 @@
 
 io.sockets.on('connection', function (socket) {
   //note the use of io.sockets to emit but socket.on to listen
+  socket.on('room', function(room) {
+    socket.join(room);
+  });
   //io.sockets.emit('status', { status: 'Conected' });
-  socket.on('reset', function (data) {
+  socket.on('input', function (data, room) {
     up = data.toUpperCase();
-    io.sockets.emit('status', { status: up });
+    io.sockets.in(room).emit('output', { status: up });
   });
 });
